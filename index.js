ea8c6ac01d23e3bdb338a32b9b5a764128282124
Derek <derekr@me.com>
2014-04-15 22:05:46 -0700

    Add styles

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 var dw = require('dumb-weights');
+var map = require('array-map');
+var each = require('foreach');
+var util = require('util');
+
+function weightClassname (val) {
+    if (val === 2.5) return 'weight-2';
+
+    return 'weight-' + val;
+};
+
+function createPlate (weight) {
+    return util.format(
+        '<div class="plate %s"><span class="weight-text">%s lbs</span></div>',
+        weightClassname(weight),
+        weight
+    );
+}
 
 function step (val) {
-    console.log(dw(val));
+    var plates = dw(val);
+
+    var $plates = map(plates, function (p) {
+        return createPlate(p);
+    });
+
+    document.querySelector('.visualization').innerHTML = $plates.join('');
 }
 
 var input = document.querySelector('[name=weight]');
@@ -22,3 +45,6 @@ down.addEventListener('click', function () {
     input.stepDown();
     step(input.value);
 });
+
+// kickoff
+step(input.value);
